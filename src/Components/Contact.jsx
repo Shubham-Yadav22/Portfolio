8f9eaef20e522823b3a556ecefb2673c381edd09
Shubham-Yadav22 <432shobhit@gmail.com>
2025-05-11 21:10:49 +0530

    Changing server

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -28,7 +28,7 @@ const Contact = () => {
   };
 
   try {
-    const res = await fetch("http://localhost:5000/send", {
+    const res = await fetch("https://backendforportfolio-production.up.railway.app/send", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
